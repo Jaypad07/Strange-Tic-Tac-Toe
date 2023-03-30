7083e37eed9907288aeba1198a88886155e4936d
Jaypad07 <jaypad07@gmail.com>
2023-03-30 07:17:09 -0700

    alpha v.102

--- script.js
+++ script.js
@@ -16,27 +16,28 @@ strtBttn.addEventListener(`click`, newGame);
 
 
 //Adds an EventListener to each div that creates an X or O when clicked.
-divElems.forEach(boxPos => {
-    boxPos.addEventListener("click", () => {
-        console.log(currentPlayer);
+divElems.forEach(divPos => {
+    divPos.addEventListener("click", () => {
         TicTacToe.switchPlayer();
+        console.log(currentPlayer);
         if (currentPlayer === TicTacToe.boardPlayers[0]) { //current player
-            symbolX(boxPos);
-        }else symbolO(boxPos);
+            symbolX(divPos);
+        }else symbolO(divPos);
         
     });
 });
 
 //FUNCTIONS
-function symbolX(pos) { //function to place symbol X on board
+function symbolX(pos) { //function to place symbol X on board, div position is passed.
     let text = document.createElement("blockquote");
     pos.appendChild(text);
     text.classList.add("symContain");
     text.innerText = "X";
     text.style.fontSize = "100px";
+
 }
 
-function symbolO(pos) { //function to place symbol O on board
+function symbolO(pos) { //function to place symbol O on board, div position is passed.
     let text = document.createElement("blockquote");
     pos.appendChild(text);
     text.classList.add("symContain");
@@ -46,69 +47,74 @@ function symbolO(pos) { //function to place symbol O on board
 
 //Clears board of all symbols and asks for player names.
 function newGame() {
+    TicTacToe.removePlayers(); 
+    TicTacToe.addPlayers();
+    currentPlayer = TicTacToe.boardPlayers[0];
+    TicTacToe.turn = 0;
+
     divElems.forEach(divEle => {
         if(divEle.hasChildNodes()) {
             divEle.removeChild(divEle.firstChild);
         }
     });
-    TicTacToe.addPlayers();
-    currentPlayer = TicTacToe.boardPlayers[0];
 }
 
 
-//Player Class
+//Function that checks if innertext of blockquotes is X or O this determines winner
+//Also all boxes will then change the letters to display Player 1 or 2 winns or Emojis?
+// function gamePoints(divPos) {
+//     divElems.forEach(divPos) {
+//         //convert HTML locations into objects to hold location and a point?
+//     }
+
+//     for (let i = 0; i < divElems.length; i++) {
+//         const  = divElems[i];
+        
+//     }
+//     if (divEle) {
+        
+//     }
+// }
+
+
 class Player {
     constructor(name, player) {
         this.name = name;
         this.player = player;
-        // if ((name.length) <= 3 && (name.length) > 1 ) {
-        //     this.name = name;
-        //     this.player = player;
-        // }else {
-            
-        //     while (!((this.name.length) <= 3 && (this.name.length) > 1 )) {
-        //         console.log(name);
-        //         console.log(this.name);
-        //         this.name = prompt("Player name must be between only be 2-3 characters");
-        //     }
-        // }  
     }
 }
 
-//gameBoard class
 class gameBoard {
     constructor() {
         this.boardPlayers = [];
+        this.turn = 0;
     }
 
     addPlayers() {  //Declares player objects and asks for player names.
-        console.log(this.boardPlayers.length);
-        if (this.boardPlayers.length > 1) {
-            this.boardPlayers.pop();
-            this.boardPlayers.pop();
-        }
+        let p1Name = prompt("Please enter Player1's name");
+        let player1 = new Player(p1Name, 1);
 
-    let p1Name = prompt("Please enter Player1's name");
-    let player1 = new Player(p1Name, 1);
-
-    let p2Name = prompt("Please enter Player2's name");
-    let player2 = new Player(p2Name, 2);
-    
-    this.boardPlayers.push(player1);
-    this.boardPlayers.push(player2);
-}
-
-    turn = 0;
-    switchPlayer() {
-        if (this.turn > 1) this.turn = 0;
-        currentPlayer = this.boardPlayers[this.turn];
-        this.turn++;
-        let totalTurns = 0
-        return currentPlayer;
+        let p2Name = prompt("Please enter Player2's name");
+        let player2 = new Player(p2Name, 2);
         
+        this.boardPlayers.push(player1);
+        this.boardPlayers.push(player2);
     }
 
+    removePlayers() { //Removes players
+        while (this.boardPlayers.length > 0) {
+            this.boardPlayers.pop();
+        }
+    }
 
+    switchPlayer() {
+        if (this.turn % 2 == 0) {
+            currentPlayer = this.boardPlayers[0];
+        }else currentPlayer = this.boardPlayers[1];
+        this.turn++;
+        let totalTurns = 0 //Silver Feature
+        return currentPlayer;
+    }
 } 
 
 let TicTacToe = new gameBoard();
